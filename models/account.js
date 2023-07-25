92eda327437b3df8ac9415ceb454c97369b834e3
Kewin Titus <kewintitus@gmail.com>
2023-07-25 22:02:22 +0530

    added account schemaModel

---
+++ models/account.js
@@ -0,0 +1,24 @@
+import { Schema, model, models } from 'mongoose';
+
+const AccountSchema = new Schema({
+  accountName: {
+    type: String,
+    required: [true, 'Account Name is required'],
+  },
+  accountType: {
+    type: String,
+    required: [true, 'Account type is required'],
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Amount is required when creating an account'],
+  },
+  user: {
+    type: String,
+    required: [true, 'Must be logged in to save account'],
+  },
+});
+
+const Account = models.Accounts || model('Accounts', AccountSchema);
+
+export default Account;
