d18a6a81fbdc502dddb2a3be8802a37efddb4181
Kewin Titus <kewintitus@gmail.com>
2023-08-20 15:39:14 +0530

    bugfix

--- Components/Charts/SpendingAnalysisCharts/IncomeTrend.jsx
+++ Components/Charts/SpendingAnalysisCharts/IncomeTrend.jsx
@@ -62,8 +62,10 @@ const IncomeTrend = ({ startDate, endDate, sessionEmail, analysisType }) => {
       {
         label: 'Income',
         data: fetchedData.values,
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        // borderColor: 'rgb(255, 99, 132)',
+        // backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
       //   {
       //     label: 'Dataset 2',
