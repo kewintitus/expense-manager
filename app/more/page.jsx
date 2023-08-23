0a3ce514426e46c6bd2c57301f0cfee5b429be30
Kewin Titus <kewintitus@gmail.com>
2023-08-23 23:18:21 +0530

    bugfix

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -1,11 +1,19 @@
+'use client';
 import PageLoadSpinner from '@/UI/loaders/PageLoadSpinner';
 import TableLoadSpinner from '@/UI/loaders/TableLoadSpinner';
-import React from 'react';
+import { useSession } from 'next-auth/react';
+import React, { useEffect, useState } from 'react';
 
 const More = () => {
+  const { data: session, status } = useSession();
+  const [userSession, setUserSession] = useState(null);
+  useEffect(() => {
+    if (status === 'unauthenticated') setUserSession(null);
+    else if (status === 'authenticated') setUserSession(session);
+  }, [session, status]);
   return (
-    // <div>under development</div>
-    <TableLoadSpinner />
+    <div>under development</div>
+    // <TableLoadSpinner />
     // <PageLoadSpinner />
   );
 };
